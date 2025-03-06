c0bab55c6cb53f093fc456de63236dfc80f2c23b
Al101020 <alex.vl.m@yandex.ru>
2025-03-06 13:33:52 +1000

    commit-07

--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,14 @@
 
 const subscribeWidget = document.querySelector('.subscribe');
 const subscribeForm = subscribeWidget.querySelector('.subscribe-form');
-// const nameInput = subscribeWidget.querySelector('.name');
-// const phoneInput = subscribeWidget.querySelector('.phone');
+const nameInput = subscribeWidget.querySelector('.name');
+const phoneInput = subscribeWidget.querySelector('.phone');
 const unsubscribeBtn = subscribeWidget.querySelector('.unsubscribe-btn');
 
 subscribeForm.addEventListener('submit', (e) => {
   e.preventDefault(); //     const body = new FormData(subscribeForm);
 
-  const body = Array.from(subscribeForm.elements)
-    .filter(({ name }) => name)
-    .map(({ name, value }) => `${name}= ${encodeURIComponent(value)}`)
-    .join('&');
+  const body = new FormData(subscribeForm);
 
   const xhr = new XMLHttpRequest();
 
@@ -23,8 +20,6 @@ subscribeForm.addEventListener('submit', (e) => {
   };
 
   xhr.open('POST', 'http://localhost:7070');
-
-  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
   xhr.send(body);//     xhr.send(body);
 });
@@ -45,12 +40,34 @@ unsubscribeBtn.addEventListener('click', (e) => {
     console.log(xhr.responseText); // console.log(xhr.readyState);
   };
 
-  xhr.open('DELETE', `http://localhost:7070/?${body}`);
+  xhr.open('DELETE', 'http://localhost:7070/?' + body);//xhr.open('DELETE', `http://localhost:7070/?${body}`);
 
   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
   xhr.send();//     xhr.send(body);
 });
+
+const uploadForm = document.querySelector('.upload-form');
+const previewImage = document.querySelector('.preview-image');
+
+uploadForm.addEventListener('submit', (e) => {
+    e.preventDefault(); //     const body = new FormData(subscribeForm);
+  
+    const body = new FormData(uploadForm);
+  
+    const xhr = new XMLHttpRequest();
+  
+    xhr.onreadystatechange = function () { // подписываемся на событие изменеия статуса запроса
+      if (xhr.readyState !== 4) return;
+  
+      console.log(xhr.responseText); // console.log(xhr.readyState);
+      previewImage.src = 'http://localhost:7070' + xhr.responseText;
+    };
+  
+    xhr.open('POST', 'http://localhost:7070/upload');
+  
+    xhr.send(body);
+  });
 
 // unsubscribeBtn.addEventListener('click', (e) => {    // console.log('unsubscribeBtn');
 //     e.preventDefault();
